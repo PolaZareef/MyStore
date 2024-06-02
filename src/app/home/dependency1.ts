abead55ec25228243368334af8aa5a6c2bf7634d
PolaZareef <98190960+PolaZareef@users.noreply.github.com>
2024-06-02 17:28:00 +0300

    Update dependency1.ts

--- src/app/home/dependency1.ts
+++ src/app/home/dependency1.ts
@@ -1,2 +1,4 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
